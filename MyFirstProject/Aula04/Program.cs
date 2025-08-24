5a3360638bccf9f35b23bc668bde7d289dbb03d3
Luiz Garrido <luizhgarrido2021@outlook.com>
2025-08-24 10:38:35 -0300

    Adicionei mais aulas e mais progamaçoes bases para aprender a sintaxe

---
+++ MyFirstProject/Aula04/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("           LOGIN          ");
+            Console.WriteLine("--------------------------");
+
+            //solicitar o nome do usuario
+
+            Console.WriteLine("Insira seu nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Nome do Usuario é {nome}");
+
+            //solicitar idade do usuario 
+
+            Console.WriteLine($"Insira sua idade");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"A idade do usuario é {age}");
+
+
+        }
+    }
+}
