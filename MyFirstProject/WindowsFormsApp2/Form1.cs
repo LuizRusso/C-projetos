b39269547c4db65a91adae1d757ddfc55243f85d
Luiz_Henrique <146493946+LuizRusso@users.noreply.github.com>
2026-02-11 22:53:55 -0300

    add new projects for datorre classes

---
+++ MyFirstProject/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nome = textBox1.Text;
+            if (nome == "")
+            {
+                MessageBox.Show("O nome é Obrigatorio!");
+                textBox1.Focus(); //todo evento tem um parenteses
+                return;
+            }
+
+            label2.Text = $"Ola {nome}!";
+        }
+    }
+}
