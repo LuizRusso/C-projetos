7c8419d69056df1993da9350da0968801fb7a3f2
Luiz_Henrique <146493946+LuizRusso@users.noreply.github.com>
2026-03-04 22:53:02 -0300

    changes add

---
+++ MyFirstProject/SistemaOne/Mapeamento/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaOne.Mapeamento
+{
+    /// <summary>
+    /// Classe de mapeamento do aluno
+    /// </summary>
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public int Idade
+        {
+            get
+            {
+                return DateTime.Today.Year - DataNascimento.Year;
+            }
+        }
+
+        public bool Ativo { get; set; }
+    }
+}
